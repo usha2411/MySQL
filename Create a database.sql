7d20fc51fb24102f942c9c54eb57395bf4c1e397
usha2411 <ushanazare@gmail.com>
2025-07-14 12:24:27 +0530

    Add files via upload

---
+++ Create a database.sql
@@ -0,0 +1,8 @@
+use world;                #data from table
+select * from city;        #retrieve data from tablecourse1
+
+#Create new database 
+CREATE DATABASE mydb;
+
+#Delete database
+Drop Database mydb;
